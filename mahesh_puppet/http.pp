aa13bf913ae9dda423facd7c361daacc55e979d6
Mahesh-Thakkannavar <mahesh.thakkannavar@emc.com>
2017-03-23 11:58:05 +0530

    adding puppet manifests

--- mahesh_puppet/http.pp
+++ mahesh_puppet/http.pp
@@ -9,5 +9,6 @@ service {'httpd':
 $http_cont = '<h1> Hello to the world of puppet </h1>'
 file { '/var/www/html/index.html':
   ensure  => 'file',
+  #path    => '/var/www/html/',
   content => $http_cont,
 }
